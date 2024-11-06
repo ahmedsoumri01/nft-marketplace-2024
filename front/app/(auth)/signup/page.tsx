bf25486679eff7155b18dfd760b0082cc63932fe
ahmed soumri <ahmedsoumri01@gmail.com>
2024-11-06 16:38:57 +0100

    make the sign in page more responsive for all devices

--- front/app/(auth)/signup/page.tsx
+++ front/app/(auth)/signup/page.tsx
@@ -4,11 +4,11 @@ import Input from "@/components/inputs/Input";
 import Button from "@/components/buttons/Button";
 export default function page() {
   return (
-    <div className="flex min-h-screen items-start w-full">
-      <div className="w-1/2 h-screen bg-[url('/assets/images/signin.png')] bg-no-repeat bg-cover"></div>
-      <div className="w-1/2 p-14">
-        <p className="font-black text-6xl py-4">Create Account</p>
-        <p className="text-2xl py-4">
+    <div className="min-h-screen items-start w-full md:flex ">
+      <div className="h-80 bg-[url('/assets/images/signin.png')] bg-no-repeat bg-cover md:w-1/2 md:h-screen"></div>
+      <div className="p-4 sm:p-8 md:w-1/2 md:p-14">
+        <p className="text-4xl font-black sm:text-6xl py-4">Create Account</p>
+        <p className="text-xl sm:text-2xl py-4">
           Welcome! enter your details and start creating, collecting and selling
           NFTs.
         </p>
