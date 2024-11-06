c9ed79d09244d1fca46b40bbe138d8eee5574499
ahmed soumri <ahmedsoumri01@gmail.com>
2024-11-06 11:44:10 +0100

    improve the responsivities in highlight nft

--- front/components/home/highlightSection/HighlightSection.tsx
+++ front/components/home/highlightSection/HighlightSection.tsx
@@ -14,12 +14,14 @@ const nftHighlight = {
 
 export default function HighlightSection() {
   return (
-    <div className={`relative min-h-screen bg-[url('/assets/images/ScreenTablet.png')] bg-no-repeat bg-cover`}>
+    <div
+      className={`relative min-h-screen bg-[url('/assets/images/ScreenTablet.png')] bg-no-repeat bg-cover`}
+    >
       {/* Blurred overlay with purple gradient shadow */}
       <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-purple-600 opacity-80 backdrop-blur-sm"></div>
 
-      <div className="container relative mx-auto p-12 flex flex-col items-center justify-between min-h-screen">
-        <div className="min-h-screen container mx-auto p-12 flex items-end justify-between">
+      <div className="container relative mx-auto p-12 flex items-center justify-between min-h-screen">
+        <div className="container mx-auto items-end justify-between md:flex lg:p-12">
           <div>
             <div className="bg-background flex items-center rounded-full px-4 py-2 gap-4 my-4 w-fit">
               <Avatar
@@ -31,16 +33,18 @@ export default function HighlightSection() {
             </div>
 
             <p className=" my-4 font-black text-5xl">{nftHighlight.nftName}</p>
-
+            <div className="py-12 md:hidden">
+            <AuctionTimer auctionEndTime={nftHighlight.auctionEndTime} />
+          </div>
             <Button
               styleType="primary"
               BtnStyle="simple"
               icon="Eye"
               text="See NFT"
-              extraClasses="!rounded-xl px-12 py-4 font-bold text-xl"
+              extraClasses="!rounded-xl w-full justify-center px-12 py-4 font-bold text-xl myt-4 md:w-auto md:px-8 md:py-2"
             />
           </div>
-          <div>
+          <div className="hidden md:block">
             <AuctionTimer auctionEndTime={nftHighlight.auctionEndTime} />
           </div>
         </div>
