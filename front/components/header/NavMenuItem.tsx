5a4a95ac16f9c9236111ee2e94a1bdae1e0834c8
ahmed soumri <ahmedsoumri01@gmail.com>
2024-11-07 11:35:31 +0100

    implement the active link

--- front/components/header/NavMenuItem.tsx
+++ front/components/header/NavMenuItem.tsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import Link from "next/link";
-
+import { usePathname } from 'next/navigation'
 type Props = {
     href : string;
     text : string;
 }
 
 const NavMenuItem = ({href,text}: Props) => {
+  const currentPath = usePathname();
   return (
     <Link
-    className='text-white  transition-all ease-in-out duration-300 hover:text-callAction'
+    className={`text-white  transition-all ease-in-out duration-300 hover:text-callAction
+      
+      ${currentPath === href ? "text-callAction" : ""}`}
     href={href}>{text}</Link>
   )
 }
