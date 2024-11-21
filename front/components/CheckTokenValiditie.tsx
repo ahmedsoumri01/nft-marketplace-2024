bb3945b88e2c227eee90317394b92e0473b2cc57
ahmed soumri <ahmedsoumri01@gmail.com>
2024-11-21 14:25:35 +0100

    add controle to check if the token valid

---
+++ front/components/CheckTokenValiditie.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { RootState } from "@/lib/reduxStore";
+import { isTokenExpired } from "@/utils/utils";
+import { logout } from "@/lib/features/auth/authSlice";
+import { useDispatch } from "react-redux";
+
+export default function CheckTokenValiditie() {
+  const { isLoggedIn, token } = useSelector((state: RootState) => state.auth);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (isLoggedIn && token && isTokenExpired(token)) {
+      dispatch(logout());
+    }
+  }, []);
+  return <div></div>;
+}
